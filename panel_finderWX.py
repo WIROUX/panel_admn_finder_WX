55f3ea7c7b05bf862a52e55514f241bf8f1b14c9
Wxsad <93474444+WIROUX@users.noreply.github.com>
2022-01-08 00:11:30 +0330

    Update panel_finderWX.py

--- panel_finderWX.py
+++ panel_finderWX.py
@@ -34,6 +34,6 @@ def Credit():
 	Space(9); print "=       *** Admin Panel WX v.10***         ="
 	Space(9); print "=       ***Script by Sad WX Virus***       ="
 	Space(9); print "=   ***@termux_script***                   ="
-    Space(9); print "= ++++++++++++++++++++++++++++++++++++++++++"
+        Space(9); print "++++++++++++++++++++++++++++++++++++++++++++"
  Credit()
 findAdmin()
