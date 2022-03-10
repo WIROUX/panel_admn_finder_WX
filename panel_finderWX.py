60d14787951e32bf7d70022d8cff3b08c3938f35
Wxsad <93474444+WIROUX@users.noreply.github.com>
2022-03-10 20:51:54 +0330

    Updated 1.1

--- panel_finderWX.py
+++ panel_finderWX.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from urllib2 import Request, urlopen, URLError, HTTPError
+from urllib2 import Request, urlopen, URLError, HTTPError,os
 
 def Space(j):
 	i = 0
@@ -12,7 +12,7 @@ def Space(j):
 
 def findAdmin():
 	f = open("link.txt","r");
-	link = raw_input("Enter Site Name \n(ex : example.com or www.example.com ): ")
+	link = raw_input("Enter Site Name \n(ex : target.com or www.target.com ): ")
 	print "\n\nAvilable links : \n"
 	while True:
 		sub_link = f.readline()
@@ -28,10 +28,11 @@ def findAdmin():
 			continue
 		else:
 			print "OK ==)>>   ",req_link
-	system('clear')
+os.system('clear')
+os.system('xdg-open https://t.me/SaDWX_TM')
 def Credit():
 	Space(9); print "++++++++++++++++++++++++++++++++++++++"
-	Space(9); print "=       *** Admin Panel WX v.1***    ="
+	Space(9); print "=       *** Admin Panel WX v1.1***    ="
 	Space(9); print "=       ***Script by Sad WX Virus***  ="
 	Space(9); print "=   ***@termux_script***              ="
         Space(9); print "++++++++++++++++++++++++++++++++++++++"
